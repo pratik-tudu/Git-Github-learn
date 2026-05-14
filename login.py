7ade3daf55b7315ffd166cc925ba0f6d06030c77
Pratik Kr Tudu <pratik2do211@gmail.com>
2026-05-14 13:29:40 +0530

    Login fixed

---
+++ login.py
@@ -0,0 +1,8 @@
+passw="123"
+input_pass="123"
+def passcheck(str_pass):
+    if(str_pass == passw):
+        print("login succesfull")
+    else:
+        print("Incorrect Pass")
+passcheck(input_pass)
