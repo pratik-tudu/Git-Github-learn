a3da63d3f657ccdc5f9fcff15a344edf89fd1b27
Pratik Kr Tudu <pratik2do211@gmail.com>
2026-05-14 13:04:06 +0530

    positive num check

--- codings/new.py
+++ codings/new.py
@@ -6,3 +6,9 @@ print("New branch created pratik-work")
 print("Later all")
 print("Doubt")
 print("Good night")
+
+def newfun(num):
+    if(num>0):
+        print("positive")
+    else:
+        print("negative")
